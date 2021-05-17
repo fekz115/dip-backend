b1c1c6842e06b8a75e6da6400380233080039cd8
Fekz115 <fekz115@gmail.com>
2021-05-18 00:10:15 +0300

    feat(controller): userController implementation

---
+++ src/main/java/org/fekz115/dip/controller/UserController.java
@@ -0,0 +1,45 @@
+package org.fekz115.dip.controller;
+
+import lombok.AllArgsConstructor;
+import org.fekz115.dip.model.User;
+import org.fekz115.dip.service.UserService;
+import org.fekz115.dip.service.exception.ActivationCodeIncorrect;
+import org.fekz115.dip.service.exception.LoginOrEmailAlreadyUsed;
+import org.fekz115.dip.service.exception.LoginOrPasswordIncorrect;
+import org.fekz115.dip.service.exception.UserNotFound;
+import org.fekz115.dip.service.request.UserServiceRequests.*;
+import org.fekz115.dip.service.response.UserServiceResponses.*;
+import org.fekz115.dip.util.JwtTokenUtil;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/user")
+@AllArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @PostMapping("/registration")
+    public RegisterUserResponse registerUser(@RequestBody RegisterUserRequest request) throws LoginOrEmailAlreadyUsed {
+        return userService.registerUser(request);
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestBody LoginUserRequest request) throws LoginOrPasswordIncorrect {
+        return jwtTokenUtil.generateToken(userService.login(request).getUser());
+    }
+
+    @PostMapping("/activate")
+    public ActivateUserResponse activate(Principal principal, String activationCode) throws UserNotFound, ActivationCodeIncorrect {
+        return userService.activate(new ActivateUserRequest(principal.getName(), activationCode));
+    }
+
+    @GetMapping("/me")
+    public User getMe(Principal principal) throws UserNotFound {
+        return userService.getUserInfo(principal.getName());
+    }
+
+}
