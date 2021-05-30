1d111a060a4fe7f0c0b457ca7f6d8a453128dc9e
Fekz115 <fekz115@gmail.com>
2021-05-30 20:44:44 +0300

    fix(qr pdf): use utf-8 in pdf

--- src/main/java/org/fekz115/dip/controller/QrController.java
+++ src/main/java/org/fekz115/dip/controller/QrController.java
@@ -6,6 +6,7 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfWriter;
 import lombok.AllArgsConstructor;
 import org.fekz115.dip.model.Article;
@@ -22,8 +23,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/api/qr")
@@ -31,12 +33,15 @@ import java.io.IOException;
 public class QrController {
 
     ArticleRepository articleRepository;
+    public static final String FONT = "fonts/arial.ttf";
 
     @GetMapping(value = "/article/{articleId}", produces = MediaType.APPLICATION_PDF_VALUE)
     ResponseEntity<ByteArrayResource> generateQrToArticle(@PathVariable int articleId) throws WriterException, DocumentException, IOException {
         var article = articleRepository.findById(articleId).orElseThrow();
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + article.getTitle() + ".pdf\"").body(new ByteArrayResource(generateDocument(article)));
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + URLEncoder.encode(article.getTitle(), StandardCharsets.UTF_8.toString()) + ".pdf\"")
+                .body(new ByteArrayResource(generateDocument(article)));
     }
 
     private BufferedImage generateQRCodeImage(String barcodeText) throws WriterException {
@@ -52,7 +57,9 @@ public class QrController {
         PdfWriter.getInstance(document, ans);
         document.open();
 
-        Font font = FontFactory.getFont(FontFactory.TIMES, 24, Font.BOLD, BaseColor.BLACK);
+        BaseFont bf = BaseFont.createFont(FONT, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+
+        Font font = new Font(bf, 18, Font.NORMAL);
         Chunk chunk = new Chunk(article.getTitle(), font);
 
         Image img = Image.getInstance(toByteArray(generateQRCodeImage("/article/" + article.getId())));
